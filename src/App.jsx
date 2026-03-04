0bba3120a9184164472626c6231d07a5caac8c81
ThijsVanLoo1 <thijs30042002@gmail.com>
2026-03-04 10:17:58 +0100

    Added styleguide to project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
+      <h1 className="text-4xl font-bold">
         Vite + React + Tailwind werkt, dus Sjoerd is een blij man!
         <div className="text-sm text-gray-500 mt-2">En dat is maar goed ook, want hij heeft er hard aan gewerkt!</div>
         <div className="grid grid-cols-10 gap-1 mt-10">
