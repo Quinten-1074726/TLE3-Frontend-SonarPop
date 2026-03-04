eab61a4f402904b37af25279a93b8779891e937c
Quinten-1074726 <1074726@hr.nl>
2026-03-04 13:14:57 +0100

    ADD: Searchbar component with MOCK data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import {createBrowserRouter, RouterProvider} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Layout from "./components/Layout.jsx";
-import Home from "./pages/home";
+import Home from "./pages/Home.jsx";
 import Statistics from "./pages/Statistics.jsx";
 import Library from "./pages/Library.jsx";
 import Error from "./pages/Error.jsx";
@@ -11,24 +11,18 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <Error />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/statistics",
-        element: <Statistics />,
-      },
-      {
-        path: "/library",
-        element: <Library />,
-      },
+      { path: "/", 
+        element: <Home /> },
+
+      { path: "/statistics", 
+        element: <Statistics /> },
+        
+      { path: "/library", 
+        element: <Library /> },
     ],
   },
 ]);
 
-function App() {
-    return <RouterProvider router={router} />;
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
