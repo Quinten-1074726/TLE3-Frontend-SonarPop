2e8c51ab32221965724ea562c9244f0790c980ad
Quinten-1074726 <1074726@hr.nl>
2026-03-09 10:46:56 +0100

    New branch feature/26/pageheader branch

---
+++ src/components/ui/PageHeader.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+function PageHeader({ title, showTitle = true }) {
+  return (
+    <header
+      className="
+        flex items-center justify-between
+        px-6 pt-10 pb-4
+      "
+    >
+      {showTitle ? (
+        <h1 className="h2 truncate pr-4">{title}</h1>
+      ) : (
+        <div />
+      )}
+
+      <Link to="/profile">
+        <div
+          className="
+            w-10 h-10
+            rounded-full
+            bg-white/20
+            border border-white/20
+            flex items-center justify-center
+          "
+        >
+          { /* avatar */ }
+        </div>
+      </Link>
+    </header>
+  );
+}
+
+export default PageHeader;
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,9 +1,11 @@
 import CardsCarousel from "../components/CardsCarousel.jsx";
+import PageHeader from "../components/ui/PageHeader.jsx";
 
 const cards = [1,2,3,4,5]; //Hoveelheid kaarten straks berekent
 function Library() {
     return(
         <>
+            <PageHeader title="Library" />
             <CardsCarousel cards={cards}/>
         </>
     )
