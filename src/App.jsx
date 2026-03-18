fce85215be9eeec70bd4a18f9d1acc927deab15c
Quinten-1074726 <1074726@hr.nl>
2026-03-18 12:22:14 +0100

    hoofdletter aanpassing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import Layout from "./components/ui/Layout.jsx";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import Statistics from "./pages/Statistics.jsx";
 import Library from "./pages/Library.jsx";
 import Error from "./pages/Error.jsx";
