856b8e0e80e8757adf90ae8ec88490f1f145d245
AirMile <zeilstramiles@gmail.com>
2026-03-18 15:06:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { inspectOverlay } from "./inspect-overlay.vite.ts";
-
 // https://vite.dev/config/
 export default defineConfig({
   server: { allowedHosts: [".trycloudflare.com"] },
   plugins: [
-    react({
-      babel: {
-        plugins: ["@react-dev-inspector/babel-plugin"],
-      },
-    }),
+    react(),
     tailwindcss(),
-    inspectOverlay(),
   ],
 });
